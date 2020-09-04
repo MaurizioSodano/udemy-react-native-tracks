0034a2ad297a22368193e7172a8448a9e52c1ff0
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2020-09-04 09:16:09 +0200

    created Scaffolds for Navigators

---
+++ src/screens/AccountScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {View, StyleSheet,Text} from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+const AccountScreen=()=>{
+    return <Text style={{fontSize:48}}>AccountScreen</Text>
+};
+
+const styles=StyleSheet.create({});
+
+export default AccountScreen;
---
+++ src/screens/SigninScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {View, StyleSheet,Text} from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+const SigninScreen=()=>{
+    return <Text style={{fontSize:48}}>SigninScreen</Text>
+};
+
+const styles=StyleSheet.create({});
+
+export default SigninScreen;
